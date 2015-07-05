ca1656841a0bdcb2b9ee6fd2b0a26466b9a40857
Aracely Rodríguez Pestana <aracelycrp@gmail.com>
2015-07-05 20:20:49 +0000

    Fuentes y creditos

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,10 +6,14 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
- res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz' });
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
+
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+})
 
 module.exports = router;
