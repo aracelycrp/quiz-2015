d34fde08a55037036c615d7146515cc207edcb17
Aracely Rodríguez Pestana <aracelycrp@gmail.com>
2015-07-21 15:27:18 +0000

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var router = express.Router();
 
@@ -16,6 +18,8 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
